c20576261179759cb0caa031886b646f2d5c7c4d
Dante Filasol Jr <142869485+dtfj03@users.noreply.github.com>
2024-04-23 07:47:17 +0800

    update patient's place of birth

--- patient/add-patient.php
+++ patient/add-patient.php
@@ -135,19 +135,48 @@
     <div class="patient-container">
         <div class="patient-data">
             <label for="patient_pobprov">Province</label><br>
-            <select name="patient_pobcity" required >
-                <option name="neg_occ">Negros Occidental</option>
-                <option name="neg_or">Negros Oriental</option>
+            <select name="patient_pobprov" id="patient_pobprov" onchange="populateCities()" required>
+                <option value="neg_occ">Negros Occidental</option>
+                <option value="neg_or">Negros Oriental</option>
             </select>
         </div>
         <div class="patient-data">
             <label for="patient_pobcity">City/Municipality</label><br>
-            <input type="text" name="patient_pobcity" required />
+            <select name="patient_pobcity" id="patient_pobcity" required>
+                <!-- Options will be populated dynamically using JavaScript -->
+            </select>
         </div>
-    </div><hr>
-
+    </div>
+    <hr>
     <input type="submit" value="Add Patient" name="add_patient" />
 </form>
+
+<script>
+    function populateCities() {
+        var province = document.getElementById("patient_pobprov").value;
+        var cityDropdown = document.getElementById("patient_pobcity");
+
+        // Clear existing options
+        cityDropdown.innerHTML = "";
+
+        // Populate options based on selected province
+        if (province === "neg_occ") {
+            var cities = ["Bacolod City", "Bago City", "Binalbagan", "Cadiz City", "Calatrava", "Candoni", "Cauayan", "Enrique B. Magalona", "Escalante City", "Himamaylan City", "Hinigaran", "Hinoba-an", "Ilog", "Isabela", "Kabankalan City", "La Carlota City", "La Castellana", "Manapla", "Moises Padilla", "Murcia", "Pontevedra", "Pulupandan", "Sagay City", "Salvador Benedicto", "San Carlos City", "San Enrique", "Silay City", "Sipalay City", "Talisay City", "Toboso", "Valladolid", "Victorias City"];
+        } else if (province === "neg_or") {
+            var cities = ["Amlan", "Ayungon", "Bacong", "Bais City", "Basay", "Bayawan City", "Bindoy", "Canlaon City", "Dauin", "Dumaguete City", "Guihulngan City", "Jimalalud", "La Libertad", "Mabinay", "Manjuyod", "Pamplona", "San Jose", "Santa Catalina", "Siaton", "Sibulan", "Tanjay City", "Tayasan", "Valencia", "Vallehermoso", "Zamboanguita"];
+        }
+
+        // Add options to the dropdown
+        for (var i = 0; i < cities.length; i++) {
+            var option = document.createElement("option");
+            option.text = cities[i];
+            cityDropdown.add(option);
+        }
+    }
+
+    // Call populateCities initially to populate cities based on the default province
+    populateCities();
+</script>
 
     </div>
 </body>
